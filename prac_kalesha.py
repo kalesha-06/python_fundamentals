9ec6637b8f738ef8ea63a5a276e6618dc5928402
kalesha-06 <75728912+kalesha-06@users.noreply.github.com>
2020-12-09 16:08:40 +0530

    Add files via upload

---
+++ prac_kalesha.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+name='kalesha'
+
+
+# In[2]:
+
+
+print(name)
+
+
+# In[3]:
+
+
+print(name.title())
+
+
+# In[4]:
+
+
+lst_name='shaik'
+
+
+# In[5]:
+
+
+type(lst_name)
+
+
+# In[6]:
+
+
+print(f{name.title()} {lst_name.title()} )
+
+
+# In[7]:
+
+
+print(f"{name.title()} {lst_name.title()}")
+
+
+# In[8]:
+
+
+frds=['kalesha','gopi','raju']
+
+
+# In[9]:
+
+
+print(frds)
+
+
+# In[10]:
+
+
+type(frds)
+
+
+# In[11]:
+
+
+print(frds[0])
+
+
+# In[12]:
+
+
+print(frds.title())
+
+
+# In[13]:
+
+
+frds.append('uday')
+
+
+# In[14]:
+
+
+print(frds)
+
+
+# In[15]:
+
+
+name.insert(1,'uday')
+
+
+# In[16]:
+
+
+name.insert(2,''sri)
+
+
+# In[17]:
+
+
+frds.insert(1,'uday')
+
+
+# In[18]:
+
+
+print(frds)
+
+
+# In[19]:
+
+
+frds.insert(1,'sri')
+
+
+# In[20]:
+
+
+print(frds)
+
+
+# In[21]:
+
+
+frds[1]='srikanth'
+
+
+# In[22]:
+
+
+print(frds)
+
+
+# In[23]:
+
+
+frds[0]='shaik'
+
+
+# In[24]:
+
+
+print(frds)
+
+
+# In[25]:
+
+
+del frds(1)
+
+
+# In[26]:
+
+
+del frds[1]
+
+
+# In[27]:
+
+
+print(frds)
+
+
+# In[28]:
+
+
+print(sorted(frds))
+
+
+# In[29]:
+
+
+del frds[1]
+
+
+# In[30]:
+
+
+print(frds)
+
+
+# In[31]:
+
+
+print(sorted(frds))
+
+
+# In[ ]:
+
+
+
+
